bd1a530677adf24e98872f63b411ba9d1cd7b612
Dawid Nowak <dawid.nowak@host.pl>
2021-05-05 21:50:26 +0200

    feat: basic component

--- App.js
+++ App.js
@@ -1,21 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+import { Appbar, TextInput, Text, Button } from 'react-native-paper';
+import { State } from 'react-native-gesture-handler';
+import { Provider as PaperProvider } from 'react-native-paper';
 
-export default function App() {
+i18n.translations = {
+  en: { 
+    header: {
+      quadraticFunction: 'Quadratic Function Calculator',
+    },
+    coefficientA: 'Coefficient A',
+    coefficientB: 'Coefficient B',
+    coefficientC: 'Coefficient C',
+    summary: 'Summary',
+    noSolutions: 'No solutions',
+    aIssue: 'Coefficient A must be not equal to 0'
+    }
+  };
+
+i18n.locale = 'en';
+i18n.fallbacks = true;
+
+const App  = (props) => {
+
+  const [factorA, setFactorA] = useState('');
+  const [factorB, setFactorB] = useState('');
+  const [factorC, setFactorC] = useState('');
+  const [summary, setSummary] = useState('');
+
+  function calculate() {
+    if (factorA != 0) {
+      let delta = Math.pow(factorB, 2) - (4 * factorA * factorC)
+
+      if (delta > 0) {
+        let x1 = ((-1 * factorB) - Math.sqrt(delta)) / 2 * factorA
+        let x2 = ((-1 * factorB) + Math.sqrt(delta)) / 2 * factorA
+  
+        setSummary("x1 = " + x1 + " x2 = " + x2)
+      } else if(delta == 0) {
+        let x1 = (-1 * factorB) / 2 * factorA
+  
+        setSummary("x = " + x1)
+      } else {
+        setSummary(i18n.t('noSolutions'))
+      }
+    } else {
+      setSummary(i18n.t('aIssue'))
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <Appbar.Header>
+          <Appbar.Content title={i18n.t('header.quadraticFunction')} />
+        </Appbar.Header>
+        <View style={[styles.container]}>
+          <View style={{ flex: 1 }}>
+            <Text style={{ fontWeight: 'bold'}}>{i18n.t('coefficientA')}</Text>
+            <TextInput
+              label={i18n.t('coefficientA')}
+              value={factorA}
+              onChangeText={factorA => setFactorA(factorA)}
+            />
+            <Text style={{ fontWeight: 'bold' }}>{i18n.t('coefficientB')}</Text>
+            <TextInput
+              label={i18n.t('coefficientB')}
+              value={factorB}
+              onChangeText={factorB => setFactorB(factorB)}
+            />
+            <Text style={{ fontWeight: 'bold' }}>{i18n.t('coefficientC')}</Text>
+            <TextInput
+              label={i18n.t('coefficientC')}
+              value={factorC}
+              onChangeText={factorC => setFactorC(factorC)}
+            />
+          </View>
+          <Button mode="contained" onPress={() => { calculate() }}>
+            Oblicz
+          </Button>
+
+          <Text>{i18n.t('summary')}: {summary}</Text>
+
+        </View>
+      </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
+
+export default App;
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+// module.exports = getDefaultConfig(__dirname);
+
+module.exports = {
+
+    resolver: {
+     sourceExts: ['jsx','js','ts','tsx'] //add here 
+    },
+  };
